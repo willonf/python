7b616342c40622c9b1336faa92b3d2170060e5f0
Willon Ferreira <contatowillon@gmail.com>
2021-12-10 06:43:58 -0400

    Collections (Default dict, ordered dict, named tuple, deque) e leitura de arquivos

---
+++ Python/seção13 - arquivos/01 leitura.py
@@ -0,0 +1,16 @@
+"""
+open() recebe o nome do arquivo e esta retorna um _io.TextIOWrapper, objeto que usamos para leitura e escrita.
+Por padrão, a função open() abre o arquivo em modo de leitura.
+O arquivo deve exister, se não ocorre o erro FileNotFoundError
+
+O método read() lê o arquivo inteiro.
+Modos: r,w,x,a,b,t,U, + (leitura e escrita)
+"""
+
+try:
+    file = open('text', mode='r', encoding='UTF-8')
+    print(file.read())
+    # print(file.read())
+
+except FileNotFoundError as error:
+    print(error.strerror)
