6464c3a285febb1165c3c2744fbe91a70d0af4d7
willon.ferreira <willon.ferreira@fpf.br>
2021-12-21 12:56:30 -0400

    Listas aninhadas

--- Python/seção9 - comprehensions/03 listas aninhadas.py
+++ Python/seção9 - comprehensions/03 listas aninhadas.py
@@ -1,1 +1,12 @@
-print('teste')
+"""
+Listas aninhadas
+
+"""
+
+
+listas = [[1,2,3], [4,5,6], [7,8,9]]
+
+[[print(valor) for valor in lista] for lista in listas]
+
+tabuleiro = [[numero for numero in range(1,4)] for valor in range(1,4)]
+print(tabuleiro)
